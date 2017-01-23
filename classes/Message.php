5864ba3bd0666e9db681e36f3e7c821f27b7c9bd
komodor8 <k.modor@yahoo.com>
2017-01-23 14:59:33 +0100

    date

--- classes/Message.php
+++ classes/Message.php
@@ -6,30 +6,16 @@ class Message{
 	private $content;
 
 	public function __construct ($a, $d, $c){
-		$this->$author = $a;
-		$this->$date = $d;
+		setlocale(LC_TIME,"fr_FR");
+		$this->author = $a;
+		$this->date = $d;
 		$this->content = $c;
+	}
 
 	public function renderHtml() {
+		$date = strftime(%d %B %Y %H %M, $this->date);
 		// on veut retourner 
 		// nom date le 23 janvier 2017 heure/min  et le message 
-		return "<h2>{$this->author} a dit : </h2> <p>{$this->content} le <time>".strftime(%d %B %Y %H %M, $this->date)."</time></p>";
+		return "<h2>{$this->author} a dit : </h2> <p>{$this->content} le <time>{strftime(%d %B %Y %H %M, $this->date)}</time></p>";
 	}
-
-
-
-
-	}
-
-
-
-
-
-
-
 }
-
-
-
-
-?>
